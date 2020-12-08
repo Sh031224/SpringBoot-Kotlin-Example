94bac7ea8c3d3eea445a074520ed93a19adf402c
Sh031224 <1cktmdgh2@gmail.com>
2020-12-08 17:22:08 +0900

    Add: settings example

---
+++ src/test/kotlin/boot/spring/example/ExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package boot.spring.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampleApplication {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
